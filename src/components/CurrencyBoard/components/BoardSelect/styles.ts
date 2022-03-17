8323d2571ecd9933076fe4a8ae11a139d39d11a9
Dmitriy Kuzmenko <dmitriy.kuzmenko.ss@gmail.com>
2022-03-17 12:01:40 +0200

    fix(board table): added mobile styles

--- src/components/CurrencyBoard/components/BoardSelect/styles.ts
+++ src/components/CurrencyBoard/components/BoardSelect/styles.ts
@@ -3,6 +3,9 @@ import { makeStyles } from '@material-ui/core';
 export const useStyles = makeStyles((theme) => ({
 	select: {
 		marginTop: 20,
-		width: 400,
+		width: 300,
+		[theme.breakpoints.up('md')]: {
+			width: 400,
+		},
 	},
 }));
--- src/components/CurrencyBoard/components/BoardTable/styles.ts
+++ src/components/CurrencyBoard/components/BoardTable/styles.ts
@@ -3,10 +3,13 @@ import { LightBlue } from '../../../../utils';
 
 export const useStyles = makeStyles((theme) => ({
 	currencyList: {
-		width: 400,
+		width: 300,
 		marginTop: 30,
 		padding: 10,
 		borderRadius: 20,
 		backgroundColor: LightBlue,
+		[theme.breakpoints.up('md')]: {
+			width: 400,
+		},
 	},
 }));
