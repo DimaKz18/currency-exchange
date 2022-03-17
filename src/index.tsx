694748d2ad8dbea6b513badfa25085505973675d
Dmitriy Kuzmenko <dmitriy.kuzmenko.ss@gmail.com>
2022-03-17 13:12:51 +0200

    feat(app): changed app to PWA

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import { i18next } from './translations/i18n';
 import { I18nextProvider } from 'react-i18next';
 import { BrowserRouter } from 'react-router-dom';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
 	<React.StrictMode>
@@ -16,3 +17,8 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
